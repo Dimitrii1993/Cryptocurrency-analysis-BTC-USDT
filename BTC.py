c0392677e80679024b3653e69f9a12278297d0af
Siberia <123393863+Dimitrii1993@users.noreply.github.com>
2025-11-14 23:20:12 +0300

    Update BTC.py

--- BTC.py
+++ BTC.py
@@ -1,1 +1,28 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+import matplotlib.dates as mdates
+import seaborn as sns 
+import plotly.express as px
 
+btc_1 = pd.read_csv('btcusd.csv')
+
+btcusd = btc_1.copy()
+
+btc_1
+
+ser_date = pd.Series(data = btcusd['date'])
+ser_open = pd.Series(data = btcusd['open'], dtype = 'int64')
+ser_close = pd.Series(data = btcusd['close'], dtype = 'int64')
+ser_volume = pd.Series(data = btcusd['Volume USD'], dtype = 'int64')
+
+btc = pd.DataFrame({
+    'date' : ser_date,
+    'open': ser_open,
+    'close': ser_close,
+    'volume': ser_volume 
+})
+
+btc['date'] = pd.to_datetime(btc['date'])
+btc.head()
